40a113a22522977601902317c805f14d1be581c7
Bruno Virginio <bruno.virginio23@gmail.com>
2019-11-11 19:55:19 -0300

    necessary files added

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -20,7 +20,7 @@ export default function Login({ history }) {
     return (
         <>
             <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
-            <form onSubmit={handleSubmit} >
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="email">E-MAIL *</label>
                 <input type="email" id="email" placeholder="Seu melhor e-mail" value={email} onChange={event => setEmail(event.target.value)}/>
                 <button className="btn" type="submit">Entrar</button>
